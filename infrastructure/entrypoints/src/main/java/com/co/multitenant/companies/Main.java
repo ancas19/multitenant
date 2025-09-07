634bd197e0df816fedf6983b8c1fd4c3e3ee0799
ancas19 <andres.cmonsalve@gmail.com>
2025-09-06 20:06:08 -0500

    Feat: Init project with architecture

---
+++ infrastructure/entrypoints/src/main/java/com/co/multitenant/companies/Main.java
@@ -0,0 +1,10 @@
+package com.co.multitenant.companies;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(CompaniesApplication.class, args);
+    }
+}
